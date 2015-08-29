eaa4aa83c0b6ca27654c79f6625dd24be82c317e
Lukas Georgieff <lukas.georgieff@hotmail.com>
2015-08-29 23:46:38 +0200

    closure-linter: Add several test scripts

---
+++ closure_tools/closure_linter/test.js
@@ -0,0 +1,26 @@
+// ====================================================================================================================
+// Copyright (C) 2015  Lukas Georgieff
+// Last modified: 08/29/2015
+// Description: A JavaScript file that contaisn some errors and invalid coding style. The purpose of this file is to
+//              illustrate the usage and limitations of the Closure Linter tools.
+// ====================================================================================================================
+
+// ====================================================================================================================
+// This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+// License as published by the Free Software Foundation, either version 3 of the License, or any later version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along with this program.
+// If not, see http://www.gnu.org/licenses/.
+// ====================================================================================================================
+
+/**
+*/
+function my_fun () {
+    return iDoesNotExist +2;
+}
+
+my_fun();

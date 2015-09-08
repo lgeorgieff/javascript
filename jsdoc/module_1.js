074bd1e6f708b3b0ec3af424a84ccea7b0d45221
Lukas Georgieff <lukas.georgieff@hotmail.com>
2015-09-08 22:40:33 +0200

    jsdoc: Add new jsdoc comments

--- jsdoc/module_1.js
+++ jsdoc/module_1.js
@@ -1,13 +1,16 @@
 /**
- * @fileOverview This file illustrates hwo to use JSDoc to generate
- *               documentation resources for JavaScript source files.
- * @author <a href="mailto:lukas.georgieff@hotmail.com">Lukas Georgieff</a>
- * @version 1.0.0
+ * @module module_1
+ * @fileOverview This file illustrates how to use JSDoc to generate documentation resources for JavaScript source
+ *               files. <br/>
+ *               In particular this file defines some logic and exports it as Node.js module.
+ * @author Lukas Georgieff <lukas.georgieff@hotmail.com>
+ * @version 1.0.1
+ * @license LGPL-3.0
  */
 
 
 /**
- * @todo Create some logi
+ * @todo Create some logic
  * @todo Expose a Node.js module
  * @todo Create a second Node.js module that references this module
  * @todo Create a entry script that uses this module and the second module

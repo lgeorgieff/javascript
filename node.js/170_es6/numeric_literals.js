98810c0950bbdd8d1dcb85bbbea6567c25950fe8
Lukas Georgieff <lukas.georgieff@hotmail.com>
2015-10-04 21:09:05 +0200

    es6: Add more examples to numeric literals

--- node.js/170_es6/numeric_literals.js
+++ node.js/170_es6/numeric_literals.js
@@ -22,13 +22,19 @@ var assert = require('assert');
 assert.strictEqual(0b11, 3);
 assert.strictEqual(0b0011, 3);
 assert.strictEqual(0B1101, 13);
+assert.strictEqual(-0B1101, -13);
+assert.strictEqual(+0B1101, +13);
 
 // === Octal numbers
 assert.strictEqual(011, 9);
 assert.strictEqual(077, 63);
+assert.strictEqual(-077, -63);
+assert.strictEqual(+077, +63);
 // If a numeric literal starts with 0 but is followed by a number > 7, it is treateds as a decimal number.
-assert.strictEqual(081, 81);
+assert.strictEqual(-081, -81);
 
 // === Hexadecimal numbers
 assert.strictEqual(0XFF, 255);
+assert.strictEqual(-0XFF, -255);
+assert.strictEqual(+0XFF, +255);
 assert.strictEqual(0x00000ABCDEF, 11259375);

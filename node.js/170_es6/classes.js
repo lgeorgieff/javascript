2edd442f0e8d820266eb2377c5c9459a37f04806
Lukas Georgieff <lukas.georgieff@hotmail.com>
2015-10-16 20:40:54 +0200

    es6: Add class example

--- node.js/170_es6/classes.js
+++ node.js/170_es6/classes.js
@@ -1,6 +1,6 @@
 // ====================================================================================================================
 // Copyright (C) 2015  Lukas Georgieff
-// Last modified: 10/03/2015
+// Last modified: 10/16/2015
 // Description: Illustrate the ES6 feature "class".
 // ====================================================================================================================
 
@@ -18,3 +18,66 @@
 
 'use strict';
 
+let assert = require('assert');
+
+// Define a class Vehicle which is used as base class for the type Car.
+class Vehicle {
+    // Maps to the constructor function.
+    constructor (speed) {
+        this._speed = speed;
+    }
+
+    // Maps to an instance method move (bind to the protype of Vehicle).
+    move (speedKmh) {
+        if (typeof speedKmh !== 'number') throw new Error('speedKmh must be a number!');
+        this._speed = speedKmh;
+    }
+
+    // Maps to a static method kmh2mph (bind to the contructor function object).
+    static kmh2mph(kmh) {
+        if (typeof kmh !== 'number') throw new Error('kmh must be a number!');
+        return kmh * 0.621371;
+    }
+};
+
+// Define a class Car which is derived from Vehicle, i.e. the prototype of Car is an instance of Vehicle.
+class Car extends Vehicle {
+    // This constructor is not called by node.js - seems to be not implemented yet.
+    constuctor (speed, brand, color) {
+        // Calling the super constructor inside a constructor is not supported by node.js yet.
+        //super(speed);
+        this._color = color;
+        this._brand = brand;
+        console.log(this);
+    }
+
+    // Maps to an instance method drive (bind to the protype of Car). Internally the method move of the super class
+    // is called.
+    drive (speedKmh) {
+        super.move(speedKmh);
+    }
+};
+
+let vehicle = new Vehicle(37);
+assert.strictEqual(vehicle._speed, 37);
+vehicle.move(199);
+assert.strictEqual(vehicle._speed, 199);
+assert.throws(function () {
+    vehicle.drive(231);
+}, TypeError);
+assert.strictEqual(vehicle._speed, 199);
+
+let bmw = new Car(123, 'BMW', 'White');
+assert.strictEqual(bmw._speed, 123);
+assert.strictEqual(bmw._color, undefined);
+assert.strictEqual(bmw._brand, undefined);
+bmw.move(210);
+assert.strictEqual(bmw._speed, 210);
+bmw.drive(231);
+assert.strictEqual(bmw._speed, 231);
+
+assert.strictEqual(Math.round(Car.kmh2mph(12) * 1000), 7.456 * 1000);
+assert.strictEqual(Math.round(Vehicle.kmh2mph(12) * 1000), 7.456 * 1000);
+assert.throws(function () {
+    bmw.kmh2mph(12);
+}, TypeError);
